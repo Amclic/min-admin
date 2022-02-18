8fbbb1c6054038c712300a8f41ca06c1c8fcaf3e
Amclic <1397738363@qq.com>
2022-02-18 11:02:42 +0800

    Revert "Revert "ADD：mockjs，env环境变量类型提示""
    This reverts commit a5da05c41cab2e634c601e09e3828c1ae7ce2a9d.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { ConfigEnv, defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import alias from './vite/alias'
 import { parseEnv } from './vite/util'
 import setupPlugins from './vite/plugins'
 
+// import vue from '@vitejs/plugin-vue' 拆分到vite插件目录下
 // export default defineConfig({
 //   plugins: [vue()],
 //   resolve: {
@@ -15,10 +15,9 @@ export default ({ command, mode }: ConfigEnv) => {
   const isBuild = command === 'build'
   const root = process.cwd()
   const env = parseEnv(loadEnv(mode, root))
-  
+
   return { 
-    plugins: [vue()],
-    // plugins: setupPlugins(isBuild, env),
+    plugins: setupPlugins(isBuild, env),
     resolve: {
       alias
     }
--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -1,3 +1,13 @@
-export default function setupPlugins(isBuild: boolean, env: Record<string, any>) {
+import vue from '@vitejs/plugin-vue'
+import { createLogger, Plugin } from 'vite'
+import setupMockPlugin  from './mock'
 
+export default function setupPlugins(isBuild: boolean, env: ViteEnv) {
+  const plugins: Plugin[] = [vue()]
+
+  console.log()
+
+  plugins.push(setupMockPlugin(isBuild))
+
+   return plugins
 }
